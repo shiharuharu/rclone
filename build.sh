a1969cd074e1e67eff26ce46bf8ee2d4cd2538ea
shiharu <shiharu@126.com>
2023-02-12 20:27:49 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v "$(pwd)":/rclone golang:latest /bin/bash -c "cd /rclone && CGO_ENABLED=0 make"
+docker run --rm -it -v "$(pwd)":/rclone golang:latest /bin/bash -c "cd /rclone && CGO_ENABLED=0 make"
